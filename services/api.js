0c62c669c86836d230edfefd65740f3ebee26fc1
marcospgcastro <marcospgcastro@fisica.if.uff.br>
2022-10-12 11:34:45 -0300

    mudança de key

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+    baseURL: 'https://dvopqmcpyjvmwnzangbk.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2b3BxbWNweWp2bXduemFuZ2JrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjU1NTM0MzcsImV4cCI6MTk4MTEyOTQzN30.K3HZl2UbeperXHVKbJyJ0XsWci1T1thetlrmHWS9OM0",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2b3BxbWNweWp2bXduemFuZ2JrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjU1NTM0MzcsImV4cCI6MTk4MTEyOTQzN30.K3HZl2UbeperXHVKbJyJ0XsWci1T1thetlrmHWS9OM0"
     }
 })

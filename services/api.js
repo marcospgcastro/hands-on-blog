24b7c6d542f64d94a2ad72126622e7ff16127954
marcospgcastro <marcospgcastro@fisica.if.uff.br>
2022-10-12 12:18:43 -0300

    Blog starter

--- services/api.js
+++ services/api.js
@@ -7,3 +7,5 @@ export const api  = axios.create({
         authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2b3BxbWNweWp2bXduemFuZ2JrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjU1NTM0MzcsImV4cCI6MTk4MTEyOTQzN30.K3HZl2UbeperXHVKbJyJ0XsWci1T1thetlrmHWS9OM0"
     }
 })
+
+// Inserido tabela própria, criada em supabase
